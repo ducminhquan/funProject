19eb57a51d822584c0d5dc27c48fba23407e28ed
Hoang Nguyen <nguyenhuyhoang83@gmail.com>
2014-10-15 19:58:36 +0700

    + Add Matlab/Octave version.

---
+++ FTS_Hackathon_1.m
@@ -0,0 +1,21 @@
+allcases = perms(1:9);
+for i = 1:size(allcases,1) 
+    array = allcases(i,:);
+    num = array(1);
+    flag = 1;
+    for j = 2:9
+        num = num*10 + array(j);
+        if (mod(num, j) ~= 0)        
+            flag = 0;
+            break;
+        end
+    end
+    if (flag == 0)
+        num = -1;
+    end
+    if (num ~= -1)
+        sprintf('RAAAAAAAAAAAAA NE: %d\n', abc)
+%         break;
+    end
+end    
+
