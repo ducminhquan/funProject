42c935a01a10aee95a3e3f52120065f99878fadd
nbang <daibang1102@gmail.com>
2014-10-16 00:13:35 +0700

    Name refactor

--- MainRecursive.java
+++ MainRecursive.java
@@ -1,18 +1,18 @@
 import java.util.Date;
 
-public class MainRescusive {
+public class MainRecursive {
 
-	private static final int BASE = 16;
+	private static final int BASE = 10;
 	private static boolean[] check = new boolean[BASE];
 
-	private static void rescursive(long base, long index, long value) {
+	private static void recursive(long base, long index, long value) {
 		if (index < base - 1) {
 			value *= base;
 			index++;
 			for (int i = 1; i < base; i++) {
 				if ((value + i) % index == 0 && !check[i]) {
 					check[i] = true;
-					rescursive(base, index, value + i);
+					recursive(base, index, value + i);
 					check[i] = false;
 				}
 			}
@@ -24,7 +24,7 @@ public class MainRescusive {
 	public static void main(String[] args) {
 		Date date = new Date();
 
-		rescursive(BASE, 0, 0);
+		recursive(BASE, 0, 0);
 
 		System.out.println(new Date().getTime() - date.getTime());
 	}
