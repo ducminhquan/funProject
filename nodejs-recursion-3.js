706839c1af0cd102719a5fd90143085e219431c4
ducminhquan <ducminhquan@gmail.com>
2014-10-15 21:48:16 +0700

    Create nodejs-recursion-3.js
    2ms version

---
+++ nodejs-recursion-3.js
@@ -0,0 +1,23 @@
+console.time('Running Time');
+var n = 9;
+var options = [];
+options[1] = options[3] = options[7] = options[9] = [1, 3, 7, 9];
+options[2] = options[4] = options[6] = options[8] = [2, 4, 6, 8];
+options[5] = [5];
+var tryNext = function(arrNum, next, i) { return i==1 || i==9 || i==5  || (( arrNum.join('') + next) % i === 0); };
+var check = function(arrNum, next, i) {	return (arrNum.indexOf(next) == -1) && tryNext(arrNum, next,i); };
+var getNum = function(result, i) {
+	if (i > n) {
+		console.log(result.join('')); 
+	} else {
+		options[i].forEach(function(j) {
+			if (check(result, j, i)) {
+				result[i-1] = j;
+				getNum(result, i + 1);
+				result[i-1] = '';			
+			};	
+		});
+	}
+};
+getNum([], 1);
+console.timeEnd('Running Time');
