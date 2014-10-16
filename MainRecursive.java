57f32412474746cf4bbc59965a16f092d7c0b7c3
nbang <daibang1102@gmail.com>
2014-10-16 14:34:11 +0700

    Update java recursive

--- MainRecursive.java
+++ MainRecursive.java
@@ -2,22 +2,33 @@ import java.util.Date;
 
 public class MainRecursive {
 
-	private static final int BASE = 10;
+	private static final int BASE = 16;
+	private static final char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 	private static boolean[] check = new boolean[BASE];
 
-	private static void recursive(long base, long index, long value) {
+	private static void recursive(int base, long index, long value) {
 		if (index < base - 1) {
 			value *= base;
 			index++;
-			for (int i = 1; i < base; i++) {
-				if ((value + i) % index == 0 && !check[i]) {
+			for (int i = 0; i < base; i++) {
+				if (!check[i] && (value + i) % index == 0) {
 					check[i] = true;
 					recursive(base, index, value + i);
 					check[i] = false;
 				}
 			}
 		} else {
-			System.out.println(value);
+			print(value, base);
+			System.out.println();
+		}
+	}
+
+	private static void print(long value, int base) {
+		if (value > base) {
+			print(value / base, base);
+			print(value % base, base);
+		} else {
+			System.out.print(chars[(int) value]);
 		}
 	}
 
